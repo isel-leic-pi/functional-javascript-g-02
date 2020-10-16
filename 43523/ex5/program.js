80347540757f9f48a07d4dab50861d7b184c9eef
joanav16 <joanav.92@gmail.com>
2020-10-16 13:09:23 +0100

    changes in ex5

--- 43523/ex5/program.js
+++ 43523/ex5/program.js
@@ -1,6 +1,38 @@
-function checkUsersValid(goodUsers) {
+/*
+## Example
+    var goodUsers = [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 }
+    ]
+    // `checkUsersValid` is the function you'll define
+    var testAllValid = checkUsersValid(goodUsers)
+
+    testAllValid([
+      { id: 2 },
+      { id: 1 }
+    ])
+    // => true
+
+    testAllValid([
+      { id: 2 },
+      { id: 4 },
+      { id: 1 }
+    ])
+    // => false
+*/ 
+
+// function checkUsersValid(goodUsers) {
+//     return function allUsersValid(submittedUsers) {
+//         return submittedUsers.every(u => goodUsers.some(u1 => u1.id==u.id))
+//     };
+//   }
+//---
+  function checkUsersValid(goodUsers) {
     return function allUsersValid(submittedUsers) {
-        return submittedUsers.every(u => goodUsers.some(u1 => u1.id==u.id))
+        const  validate = (u1,u2) => u1 == u2;
+        return submittedUsers.every( u => goodUsers.some(u1 => validate(u.id,u1.id)));
+
     };
   }
 
