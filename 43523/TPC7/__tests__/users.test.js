25020267f4168f509dbab2883414caeca923f2a9
joanav16 <joanav.92@gmail.com>
2020-11-17 20:18:56 +0000

    tpc7

---
+++ 43523/TPC7/__tests__/users.test.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-undef */
+'use strict'
+
+const users = require('../lib/users')
+
+
+test('test users module getUsers successfuly', done => {
+    users.getUser('gamboa', (err, user) => {
+        // Assert that there is no error
+        expect(err).toBeFalsy()
+
+        // Assert that property username of user is gamboa
+        expect(user.username).toBe('gamboa')
+        done()
+    })
+})
+
+test('test users module getUser not exists', done => {
+    users.getUser('Teresa', (err, user) => {
+        // Assert that there is no error
+        //expect(err).toBeTruthy
+        expect(err).toBeDefined
+
+        done()
+    })
+})
+
+test('test users module Adds existing user', (done) => {
+    users.addUser('gamboa', (err,user) => {
+        if(err) { 
+            expect(err.toString()).toBe('Error: Exists already a username with the name gamboa')
+            expect(user).toBeUndefined()
+        } else fail()
+
+        done()
+    })
+})
+
+ test('test users module Adds not existing user', (done) => {
+
+    users.addUser('c', (err,user) => {
+        expect(err).toBeFalsy()
+        expect(user).toBe('c')
+        done()
+    })
+}) 
+
+test('test users module Adds artist', (done) => {
+    users.addArtist('Gamboa','muse',(err,artist) => {
+        expect(err).toBeFalsy()
+        expect(artist).toBe('muse')
+        done()
+    })
+})
+
+
